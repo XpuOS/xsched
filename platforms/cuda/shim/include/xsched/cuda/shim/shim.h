82636df956f35573239c9b09f1c2c30d8ebcc509
tonghx <260526071@qq.com>
2026-06-30 10:35:13 +0800

    修改cuda平台默认流拦截机制

--- platforms/cuda/shim/include/xsched/cuda/shim/shim.h
+++ platforms/cuda/shim/include/xsched/cuda/shim/shim.h
@@ -6,6 +6,7 @@
 #include "xsched/cuda/hal/common/driver.h"
 #include "xsched/cuda/hal/common/handle.h"
 #include "xsched/cuda/hal/common/cuda_command.h"
+#include "xsched/cuda/hal.h"
 
 namespace xsched::cuda
 {
