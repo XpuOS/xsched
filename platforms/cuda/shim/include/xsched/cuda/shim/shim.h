d5248118dc62a2e02599fa1feaa274d2a3abe801
tonghx <260526071@qq.com>
2026-07-02 16:11:55 +0800

    cuda调度策略修改

--- platforms/cuda/shim/include/xsched/cuda/shim/shim.h
+++ platforms/cuda/shim/include/xsched/cuda/shim/shim.h
@@ -17,17 +17,7 @@ inline CUresult X##name(FOR_EACH_PAIR_COMMA(DECLARE_PARAM, __VA_ARGS__), CUstrea
     if (stream == 0) { \
         WaitBlockingXQueues(); \
     } \
-    auto xq = xsched::preempt::HwQueueManager::GetXQueue(GetHwQueueHandle(stream)); \
-    if (xq == nullptr) { \
-        xsched::preempt::XQueueManager::AutoCreate([&](HwQueueHandle *hwq) -> XResult { \
-            return CudaQueueCreate(hwq, stream); \
-        }); \
-        xq = xsched::preempt::HwQueueManager::GetXQueue(GetHwQueueHandle(stream)); \
-    } \
-    if (xq == nullptr) return Driver::name(FOR_EACH_PAIR_COMMA(DECLARE_ARG, __VA_ARGS__), stream); \
-    auto hw_cmd = std::make_shared<cmd>(FOR_EACH_PAIR_COMMA(DECLARE_ARG, __VA_ARGS__)); \
-    xq->Submit(hw_cmd); \
-    return CUDA_SUCCESS; \
+    return Driver::name(FOR_EACH_PAIR_COMMA(DECLARE_ARG, __VA_ARGS__), stream); \
 }
 
 void WaitBlockingXQueues();
